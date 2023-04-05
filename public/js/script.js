76d1531d1378ca0b26bbc63d756bf17779bbcdfb
Armin Ghasemi <datarivers.org@gmail.com>
2023-04-05 21:19:33 +0200

    adds trello starter kit

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,32 @@
 // Set Trello app key variable
 var TRELLO_APP_KEY = 'b1861ec05e3061c705a49737619af812';
 
+
+/* global TrelloPowerUp */
+
+var Promise = TrelloPowerUp.Promise;
+
+// var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+//
+// TrelloPowerUp.initialize({
+//     // Start adding handlers for your capabilities here!
+//     // 'card-buttons': function(t, options) {
+//     // return t.set("member", "shared", "hello", "world")
+//     // .then(function(){
+//     // 	  return [{
+//     // icon: BLACK_ROCKET_ICON,
+//     // 		  text: 'Estimate Size',
+//     //       callback: function(t) {
+//     //         return t.popup({
+//     //           title: "Estimation",
+//     //           url: 'estimate.html',
+//     //         });
+//     //       }
+//     // 	  }];
+//     // })
+//     // },
+// });
+
 // Declare global variables for power-up settings
 var badgePrefix;
 var badgeColor;
@@ -84,3 +110,4 @@ window.TrelloPowerUp.initialize({
             });
     },
 });
+
